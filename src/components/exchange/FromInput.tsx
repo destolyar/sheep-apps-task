9d654c539b107e5bb77ba237cb603f572d72107b
destolyar <dead.stolyar@gmail.com>
2022-03-26 23:50:15 +0300

    fix: input bug fixed

--- src/components/exchange/FromInput.tsx
+++ src/components/exchange/FromInput.tsx
@@ -4,7 +4,7 @@ export const FromInput: React.FunctionComponent<InputProps> = (props) => {
   return(
     <input className="exchange__inputs__input" type="number" defaultValue={400} value={props.fromInputValue} onChange={(e) => {
       props.onChangeFromInput(e.target.value)
-      props.onChangeToInput((+e.target.value / props.price).toFixed(2).toString())
+      props.onChangeToInput((+e.target.value * props.price).toFixed(2).toString())
     } 
     }/>
   )
--- src/components/exchange/ToInput.tsx
+++ src/components/exchange/ToInput.tsx
@@ -4,7 +4,7 @@ export const ToInput: React.FunctionComponent<InputProps> = (props) => {
   return(
     <input className="exchange__inputs__input" type="number" defaultValue={400} value={props.toInputValue} onChange={(e) => {
       props.onChangeToInput(e.target.value)
-      props.onChangeFromInput((+e.target.value * props.price).toFixed(2).toString())
+      props.onChangeFromInput((+e.target.value / props.price).toFixed(2).toString())
     }}/>
   )
 }
